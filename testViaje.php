55f983d847e4c09ca5de8c78443a1519bb7388a4
santylucero <santiago.lucero@est.fi.uncoma.edu.ar>
2023-03-31 12:43:50 -0300

    casi terminando

--- testViaje.php
+++ testViaje.php
@@ -4,7 +4,7 @@ include("viajeFeliz.php");
 $codigo = "";
 $destino = "";
 $cantPasajeros = 0;
-$personasViaje = [];
+$pasajeros = [];
 $nombre = "";
 $apellido = "";
 $dni = "";
@@ -19,13 +19,13 @@ function datosPasajeros($cantPas)
     $cont = 1;
     $arrayPas = [];
     do {
-        echo "ingrese nombre del pasajero: ";
+        echo "ingrese nombre del pasajero: \n";
         $nombrePas = trim(fgets(STDIN));
-        echo "ingrese el apellido: ";
+        echo "ingrese el apellido:\n ";
         $apellidoPas = trim(fgets(STDIN));
-        echo "ingrese el numero de documento: ";
+        echo "ingrese el numero de documento:\n ";
         $dniPas = trim(fgets(STDIN));
-        array_push($arrayPas, array("nombre: " => $nombrePas, "apellido: " => $apellidoPas, "DNI: " => $dniPas));
+        array_push($arrayPas, array("nombre: " => $nombrePas, "apellido: " => $apellidoPas, "DNI: " => $dniPas . "\n\n"));
         $cont++;
     } while ($cont == $cantPas);
     return $arrayPas;
@@ -34,23 +34,102 @@ function datosPasajeros($cantPas)
 /**
  * muestra un menu y retorna en las opciones
  */
-function menu(){
-    echo "[1]VER DATOS DEL VIAJE";
-    echo "[2]MODIFICAR INFORMACION DEL VIAJE";
-    echo "[3]MODIFICAR LOS DATOS DE UNA PERSONA";
-    echo "[4]SALIR";
-    $opcSeleccionada=trim(fgets(STDIN));
+function menu()
+{
+    echo "\n";
+    echo "SELECCIONE ALGUNA OPCION\n";
+    echo "[1]VER DATOS DEL VIAJE\n";
+    echo "[2]MODIFICAR INFORMACION DEL VIAJE\n";
+    echo "[3]MODIFICAR LOS DATOS DE UNA PERSONA\n";
+    echo "[4]SALIR\n\n";
+    $opcSeleccionada = trim(fgets(STDIN));
     return $opcSeleccionada;
 }
 
-echo "ingrese codigo de viaje: ";
+
+echo "ingrese codigo de viaje:\n ";
 $codigo = trim(fgets(STDIN));
-echo "destino del viaje: ";
+echo "destino del viaje:\n ";
 $destino = trim(fgets(STDIN));
-echo "ingrese la cantidad de pasajeros: ";
+echo "ingrese la cantidad de pasajeros:\n ";
 $cantPasajeros = trim(fgets(STDIN));
-echo "INGRESE LOS DATOS DE LOS PASAJEROS ";
+echo "INGRESE LOS DATOS DE LOS PASAJEROS\n\n ";
 $datosPas = datosPasajeros($cantPasajeros);
+$viajeFeliz = new ViajeFeliz($codigo, $destino, $cantPasajeros, $datosPas);
 
-$viajeFeliz= new ViajeFeliz($codigo, $destino, $cantPasajeros, $datosPas);
+$viajeFeliz->get_codigo($codigo);
+$viajeFeliz->get_destino($destino);
+$viajeFeliz->get_cantPasajeros($cantPasajeros);
+$viajeFeliz->get_pasajeros($pasajeros);
 
+do {
+    $opcionSel = menu();
+    switch ($opcionSel) {
+
+
+
+        case 1:
+            echo "--------------DATOS DEL VIAJE--------------\n";
+            echo "CODIGO DE VIAJE: " . $viajeFeliz->get_codigo() . "\n";
+            echo "DESTINO DE VIAJE: " . $viajeFeliz->get_destino() . "\n";
+            echo "CANTIDAD DE PASAJEROS: " . $viajeFeliz->get_cantPasajeros() . "\n";
+            echo "-------------------------------------------\n\n";
+            break;
+
+
+
+        case 2:
+            echo "MODIFICAR INFORMACION DEL VIAJE: \n
+    1- MODIFICAR CODIGO \n
+    2- MODIFICAR DESTINO \n
+    3- MODIFICAR CANTIDAD DE PASAJEROS \n\n";
+            $opcionModif = trim(fgets(STDIN));
+            switch ($opcionModif) {
+                case 1:
+                    echo "ingrese nuevo codigo: \n";
+                    $nuevoCodigo = trim(fgets(STDIN));
+                    $viejoCodigo = $viajeFeliz->get_codigo();
+                    if ($nuevoCodigo !== $viejoCodigo) {
+                        echo "El antiguo codigo era: " . $viejoCodigo . "\n";
+                        echo "El nuevo es: " . $nuevoCodigo . "\n\n";
+                        $viajeFeliz->set_codigo($nuevoCodigo);
+                    } else {
+                        echo "es el mismo codigo\n\n";
+                    }
+                    break;
+                case 2:
+                    echo "ingrese nuevo destino: \n";
+                    $nuevoDestino = trim(fgets(STDIN));
+                    $viejoDestino = $viajeFeliz->get_destino();
+                    if ($nuevoDestino !== $viejoDestino) {
+                        echo "El antiguo destino era: " . $viejoDestino . "\n";
+                        echo "El nuevo es: " . $nuevoDestino . "\n\n";
+                        $viajeFeliz->set_destino($nuevoDestino);
+                    } else {
+                        echo "es el mismo destino\n\n";
+                    }
+                    break;
+                case 3:
+                    echo "modifique la cantidad de pasajeros: \n";
+                    $nuevaCantidad = trim(fgets(STDIN));
+                    $viejaCantidad = $viajeFeliz->get_cantPasajeros($cantPasajeros);
+                    if ($nuevaCantidad !== $viejaCantidad) {
+                        echo "la antigua cantidad era: " . $viejaCantidad . "\n";
+                        echo "la nueva es: " . $nuevaCantidad . "\n\n";
+                        $viajeFeliz->set_cantPasajeros($nuevaCantidad);
+                    } else {
+                        echo "es la misma cantidad\n\n";
+                    }
+            }
+            break;
+
+
+            
+
+            case 3:
+                echo "MODIFICAR DATOS DE UNA PERSONA: \n";
+                echo "ingrese el DNI de la persona que busca modificar sus datos: ";
+                $dniModif = trim(fgets(STDIN));
+                echo "\n";
+    }
+} while ($opcionSel != "4");
--- viajeFeliz.php
+++ viajeFeliz.php
@@ -2,13 +2,13 @@
 class ViajeFeliz{
 private $codigo ;
 private $destino;
-private $cantMaxPasajeros;
+private $cantPasajeros;
 private $pasajeros=[];
 
-public function __construct($codigo,$destino,$cantMaxPasajeros,$pasajeros){
+public function __construct($codigo,$destino,$cantPasajeros,$pasajeros){
 $this->codigo=$codigo;
 $this->destino=$destino;
-$this->cantMaxPasajeros=$cantMaxPasajeros;
+$this->cantPasajeros=$cantPasajeros;
 $this->pasajeros=$pasajeros;
  }
 public function get_codigo(){
@@ -17,8 +17,8 @@ return $this->codigo;
  public function get_destino(){
 return $this->destino;
  }   
-public function get_cantMaxPasajeros(){
-return $this->cantMaxPasajeros;
+public function get_cantPasajeros(){
+return $this->cantPasajeros;
 }
 public function get_pasajeros(){
 return $this->pasajeros;
@@ -30,8 +30,8 @@ public function set_codigo($codigo){
 public function set_destino($destino){
     $this->destino=$destino;
 }
-public function set_cantMaxPasajeros($cantMaxPasajeros){
-    $this->cantMaxPasajeros=$cantMaxPasajeros;
+public function set_cantPasajeros($cantPasajeros){
+    $this->cantPasajeros=$cantPasajeros;
 }
 public function set_pasajeros($pasajeros){
     $this->pasajeros=$pasajeros;
@@ -39,7 +39,7 @@ public function set_pasajeros($pasajeros){
 public function __toString(){
     return "codigo: " . $this->codigo . " ".
          "destino: " . $this->destino . " ".
-         "cantidad maxima de pasajeros: " . $this->cantMaxPasajeros . " ". 
+         "cantidad maxima de pasajeros: " . $this->cantPasajeros . " ". 
          "pasajeros: " . $this->pasajeros . " "; 
 }
 }
